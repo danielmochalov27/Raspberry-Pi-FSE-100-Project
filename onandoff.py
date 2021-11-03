8555dadd26f2801bfecac8ecfc2aca8bad8cb58f
Margaret Wade <>
2021-11-03 15:25:30 +0000

    Unnecessary PBuzzPin function remove. Print checks added throughout.

--- onandoff.py
+++ onandoff.py
@@ -31,80 +31,90 @@ GPIO.setup(UltraTrig, GPIO.OUT)
 #Ultra Sonic Sensor output is connected to Pin 16
 
 
-def distance():
-  # Send 10 microsecond pulse to TRIGGER
-  GPIO.output(UltraTrig, True) # set TRIGGER to HIGH
-  time.sleep(0.00001) # wait 10 microseconds
-  GPIO.output(UltraTrig, False) # set TRIGGER back to LOW
+def ultra_ir_distance():
+ GPIO.output(UltraTrig, True)
+ time.sleep(0.00001)
  
-  # Create variable start and assign it current time
-  start = time.time()
-  # Create variable stop and assign it current time
-  stop = time.time()
-  # Refresh start value until the ECHO goes HIGH = until the wave is send
-  while GPIO.input(UltraEcho) == 0:
-    start = time.time()
+ GPIO.output(UltraTrig, False)
  
-  # Assign the actual time to stop variable until the ECHO goes back from HIGH to LOW = the wave came back
-  while GPIO.input(UltraEcho) == 1:
-    stop = time.time()
+ startTime = time.time()
+ endTime = time.time()
  
-  # Calculate the time it took the wave to travel there and back
-  measuredTime = stop - start
-  # Calculate the travel distance by multiplying the measured time by speed of sound
-  distanceBothWays = measuredTime * 33112 # cm/s in 20 degrees Celsius
-  # Divide the distance by 2 to get the actual distance from sensor to obstacle
-  distance = distanceBothWays / 2
+ while GPIO.input(UltraEcho) == 0:
+   startTime = time.time()
+ while GPIO.input(UltraEcho) == 1:
+   endTime = time.time()
+ 
+ during = endTime - startTime
+ ultra_distance = (during * 340)/2
+ 
+ pir_sense = 0
 
-  # Print the distance to see if everything works correctly
-  print("Distance : {0:5.1f}cm".format(distance))
-  # Return the actual measured distance
-  return distance
+ if GPIO.input(PIRPin):
+  pir_sense = 1
+ else:
+  pir_sense = 0
+
+ ultra_ir_sense = [ultra_distance, pir_sense]
+ return ultra_ir_sense
 
   # Calculates the frequency of beeping depending on the measured distance
 def beep_freq():
   # Measure the distance
-  dist = distance()
+  ultra_ir_list = ultra_ir_distance()
   # If the distance is bigger than 50cm, we will not beep at all
-  if dist > 50:
+  if ultra_ir_list[0] > 50:
     return -1
   # If the distance is between 50 and 30 cm, we will beep once a second
-  elif dist <= 50 and dist >=30:
+  elif ultra_ir_list[0] <= 50 and ultra_ir_list[0] >=30:
     return 1
   # If the distance is between 30 and 20 cm, we will beep every twice a second
-  elif dist < 30 and dist >= 20:
+  elif ultra_ir_list[0] < 30 and ultra_ir_list[0] >= 20:
     return 0.5
   # If the distance is between 20 and 10 cm, we will beep four times a second
-  elif dist < 20 and dist >= 10:
+  elif ultra_ir_list[0] < 20 and ultra_ir_list[0] >= 10:
     return 0.25
   # If the distance is smaller than 10 cm, we will beep constantly
   else:
     return 0
 
+def PBuzzBeep2(x):
+  GPIO.output(PBuzzPin2, GPIO.LOW)
+  time.sleep(x)
+  GPIO.output(PBuzzPin2, GPIO.HIGH)
+
 while True:
     # waiting for button press
     while GPIO.input(BtnPin) == 1:
-        time.sleep(0.2)
+     time.sleep(0.2)
 
-    print ("plz")
+    print ("Sensors ON")
     while GPIO.input(BtnPin) == 1:
+    
+      ultra_ir_list = ultra_ir_distance()
+      pir_sense1 = ultra_ir_list[1]
+
+      if ultra_ir_list[1] == 1:
+        PBuzzBeep2(0.5)
+        print("pir detected")
 
       freq = beep_freq()
       # No beeping
       if freq == -1:
         GPIO.output(PBuzzPin, False)
         time.sleep(0.25)
+        print("ultra not detected")
+
         # Constant beeping
       elif freq == 0:
         GPIO.output(PBuzzPin, True)
         time.sleep(0.25)
+        print("ultra detected far")
         # Beeping on certain frequency
       else:
         GPIO.output(PBuzzPin, True)
         time.sleep(0.2) # Beep is 0.2 seconds long
         GPIO.output(PBuzzPin, False)
         time.sleep(freq) # Pause between beeps = beeping frequency
-
-    
-        
-
+        print("ultra detected")
+      print("Sensors OFF")
